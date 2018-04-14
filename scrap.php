ad9e77d773e942cdb8875339fd40d4dfe0463943
Syu93 <herve.tutuaku@gmail.com>
2018-04-14 05:28:37 +0200

    [init] init project

---
+++ scrap.php
@@ -0,0 +1,59 @@
+<?php
+
+function getImage($url, $path) {
+    $ch = curl_init($url);
+    $fp = fopen($path, 'wb');
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+}
+
+$baseMetroUrl = "https://www.ratp.fr/sites/default/files/network/metro/";
+$metroImage = "https://www.ratp.fr/sites/default/files/network/metro/symbole.svg";
+
+$baseRERUrl = "https://www.ratp.fr/sites/default/files/network/rer/";
+$RERImage = "https://www.ratp.fr/sites/default/files/network/rer/symbole.svg";
+
+$baseTramUrl = "https://www.ratp.fr/sites/default/files/network/tram/";
+$tramImage = "https://www.ratp.fr/sites/default/files/network/tram/symbole.svg";
+
+// Get Metro images
+getImage($metroImage, './images/ratp/metro.svg');
+for ($i=1; $i < 15; $i++) { 
+    $name = 'ligne'. $i .'.svg';
+    if ($i == 3) {
+        $name = 'ligne'. $i .'b.svg';
+        getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+        continue;
+    }
+    if ($i == 7) {
+        $name = 'ligne'. $i .'b.svg';
+        getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+        continue;
+    }
+    getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+}
+
+// Get RER images
+$letters = range('A', 'E');
+getImage($RERImage, './images/ratp/rer.svg');
+foreach ($letters as $letter) {
+    $name = 'ligne'. $letter .'.svg';
+    getImage($baseRERUrl . $name, './images/ratp/rer/' . $name);
+}
+
+// Get Tram images
+getImage($tramImage, './images/ratp/tram.svg');
+for ($i=1; $i < 9; $i++) {
+    if ($i == 3) {
+        $name = 'ligne'. '3a' .'.svg';
+        getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+        $name = 'ligne'. '3b' .'.svg';
+        getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+        continue;
+    }
+    $name = 'ligne'. $i .'.svg';
+    getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+}
---
+++ server/cron.js
@@ -0,0 +1,85 @@
+const { google } = require("googleapis");
+const axios = require('axios');
+const http = require("https");
+
+// Load the service account key JSON file.
+const serviceAccount = require("./config.json");
+
+// Define the required scopes.
+const  scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/firebase.database"
+];
+
+// Authenticate a JWT client with the service account.
+const jwtClient = new google.auth.JWT(
+  serviceAccount.client_email,
+  null,
+  serviceAccount.private_key,
+  scopes
+);
+
+// Use the JWT client to generate an access token.
+jwtClient.authorize(function(error, tokens) {
+  if (error) {
+    console.log("Error making request to generate access token:", error);
+  } else if (tokens.access_token === null) {
+    console.log("Provided service account does not have permission to generate access tokens");
+  } else {
+    const accessToken = tokens.access_token;
+
+    // See the "Using the access token" section below for information
+    // on how to use the access token to send authenticated requests to
+    // the Realtime Database REST API.
+    // console.log("token : ", accessToken);
+    axios.get(`${serviceAccount.httpEndpoint}/users.json?access_token=${accessToken}`, {responseType: 'json'})
+      .then(response => {
+        const users = response.data;
+        return tokens = Object.keys(users).map((key) => { return users[key].token });
+      })
+      .then(tokens => {
+        console.log(tokens);
+        tokens.forEach(token => {
+          if (token == "null") return;
+          var options = {
+            "method": "POST",
+            "hostname": "fcm.googleapis.com",
+            "port": null,
+            "path": "/fcm/send",
+            "headers": {
+              "authorization": `key=${serviceAccount.token}`,
+              "content-type": "application/json",
+              "cache-control": "no-cache",
+            }
+          };
+
+          var req = http.request(options, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+              chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+              var body = Buffer.concat(chunks);
+              console.log(JSON.parse(body.toString()).success);
+            });
+          });
+
+          req.write(JSON.stringify({
+            to: token,
+            notification: {
+              title: '🚅 Hello World',
+              body: 'This is a notification',
+              click_action: "https://parisgreve.info"
+            } 
+          }));
+          req.end();
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+});
