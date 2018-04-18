d718b5a7d71ba8818d294bef53a49dff49a29225
Syu93 <herve.tutuaku@gmail.com>
2018-04-19 00:47:41 +0200

    [feature] add more info

--- scrap.php
+++ scrap.php
@@ -19,41 +19,51 @@ $RERImage = "https://www.ratp.fr/sites/default/files/network/rer/symbole.svg";
 $baseTramUrl = "https://www.ratp.fr/sites/default/files/network/tram/";
 $tramImage = "https://www.ratp.fr/sites/default/files/network/tram/symbole.svg";
 
+$baseTransilienUrl = "https://www.transilien.com/tricharte/img/picto/";
+$transilienImage = "https://www.transilien.com/tricharte/img/picto/tn-icon-transilien.svg";
+
 // Get Metro images
-getImage($metroImage, './images/ratp/metro.svg');
-for ($i=1; $i < 15; $i++) { 
-    $name = 'ligne'. $i .'.svg';
-    if ($i == 3) {
-        $name = 'ligne'. $i .'b.svg';
-        getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
-        continue;
-    }
-    if ($i == 7) {
-        $name = 'ligne'. $i .'b.svg';
-        getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
-        continue;
-    }
-    getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
-}
+// getImage($metroImage, './images/ratp/metro.svg');
+// for ($i=1; $i < 15; $i++) { 
+//     $name = 'ligne'. $i .'.svg';
+//     if ($i == 3) {
+//         $name = 'ligne'. $i .'b.svg';
+//         getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+//         continue;
+//     }
+//     if ($i == 7) {
+//         $name = 'ligne'. $i .'b.svg';
+//         getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+//         continue;
+//     }
+//     getImage($baseMetroUrl . $name, './images/ratp/metro/' . $name);
+// }
 
-// Get RER images
-$letters = range('A', 'E');
-getImage($RERImage, './images/ratp/rer.svg');
+// // Get RER images
+// $letters = range('A', 'E');
+// getImage($RERImage, './images/ratp/rer.svg');
+// foreach ($letters as $letter) {
+//     $name = 'ligne'. $letter .'.svg';
+//     getImage($baseRERUrl . $name, './images/ratp/rer/' . $name);
+// }
+
+// // Get Tram images
+// getImage($tramImage, './images/ratp/tram.svg');
+// for ($i=1; $i < 9; $i++) {
+//     if ($i == 3) {
+//         $name = 'ligne'. '3a' .'.svg';
+//         getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+//         $name = 'ligne'. '3b' .'.svg';
+//         getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+//         continue;
+//     }
+//     $name = 'ligne'. $i .'.svg';
+//     getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+// }
+
+getImage($transilienImage, './images/ratp/transilien.svg');
+$letters = ['h', 'j', 'k', 'l', 'n', 'p', 'u'];
 foreach ($letters as $letter) {
-    $name = 'ligne'. $letter .'.svg';
-    getImage($baseRERUrl . $name, './images/ratp/rer/' . $name);
-}
-
-// Get Tram images
-getImage($tramImage, './images/ratp/tram.svg');
-for ($i=1; $i < 9; $i++) {
-    if ($i == 3) {
-        $name = 'ligne'. '3a' .'.svg';
-        getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
-        $name = 'ligne'. '3b' .'.svg';
-        getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
-        continue;
-    }
-    $name = 'ligne'. $i .'.svg';
-    getImage($baseTramUrl . $name, './images/ratp/tram/' . $name);
+    $name = 'tn-icon-tra' . $letter . '.svg';
+    getImage($baseTransilienUrl . $name, './images/ratp/transilien/' . $name);
 }
---
+++ server/scrap_cron.js
@@ -0,0 +1,43 @@
+// https://codeburst.io/an-introduction-to-web-scraping-with-node-js-1045b55c63f7
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const jsonfile = require('jsonfile');
+
+// Small helper to return evaluable HTML via cheerio
+const options = {
+  uri: `https://www.transilien.com/#info-trafic-temps-reel`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/\t/g, "&nbsp;")
+         .replace(/\n/g, "");
+}
+
+const lines = ['H', 'J', 'K', 'L', 'N', 'P', 'U'];
+const data = {disruption: []};
+
+// Make the request
+rp(options)
+    .then(($) => {
+        lines.forEach((line) => {
+            let html = $('#b_info_trafic').find(`#detail-trafic-${line}`);
+            let status = html.find('h3').find('span:last-child').text();
+            data.disruption.push({
+                line: `${line}`,
+                status: status,
+            });
+        });
+        return data;
+    })
+    .then(data => {
+        let file = '/home/nimda/app/json/data.json';
+        jsonfile.writeFile(file, data, {spaces: 2}, function(err) {console.error(err);});
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
